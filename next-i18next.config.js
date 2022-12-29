867fcdf79be6fa72cecfe8469ec8557b0eaf3974
Daria <daria.diachkova@mail.ru>
2022-12-29 17:52:35 +0100

    feat(app): extend set up

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,5 @@
 // @ts-check
 
-/**
- * @type {import('next-i18next').UserConfig}
- */
 module.exports = {
   // https://www.i18next.com/overview/configuration-options#logging
   debug: process.env.NODE_ENV === 'development',
@@ -17,12 +14,4 @@ module.exports = {
       : '/locales',
 
   reloadOnPrerender: process.env.NODE_ENV === 'development',
-
-  /**
-   * @link https://github.com/i18next/next-i18next#6-advanced-configuration
-   */
-  // saveMissing: false,
-  // strictMode: true,
-  // serializeConfig: false,
-  // react: { useSuspense: false }
 }
