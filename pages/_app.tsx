77961a980d50b062cd91c27b8fb1a61fb6cc4fc9
Daria <daria.diachkova@mail.ru>
2022-12-29 20:32:20 +0100

    style main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import 'styles/content.css'
 import 'styles/layout.css'
+import 'public/assets/fonts/fonts.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
